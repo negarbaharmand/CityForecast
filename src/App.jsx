bbb93f111d17f1e0229c193fb26b4a6be54fd266
Negar Baharmand <negarbaharmand@outlook.com>
2023-06-08 17:28:55 +0200

    Add apiKey to .env file

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import axios from axios;
 
 function App() {
+ 
 
-  // const url = `http://api.openweathermap.org/geo/1.0/direct?q=dallas&appid=b4afec7fb248240ff4d1da23cf6859d4
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=dallas&appid=${import.meta.env.VITE_API_KEY}
   // `;
   return <></>;
 }
