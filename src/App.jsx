3f1506ff2df22775f67967ae0db8934365799462
Negar Baharmand <negarbaharmand@outlook.com>
2023-06-14 17:55:23 +0200

    Create app structure divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 import { useState } from "react";
-import axios from axios;
+// import axios from axios;
 
 function App() {
- 
-
-  const url = `http://api.openweathermap.org/geo/1.0/direct?q=dallas&appid=${import.meta.env.VITE_API_KEY}
-  // `;
-  return <></>;
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${import.meta.env.VITE_API_KEY} `;
+  return (
+    <>
+      <div className="app">
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>Dallas</p>
+            </div>
+            <div className="temp">
+              <h1>60°F</h1>
+            </div>
+            <div className="description">
+              <p>Clouds</p>
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="feel">
+              <p>60°F</p>
+            </div>
+            <div className="humidity">
+              <p>20%</p>
+            </div>
+            <div className="wind">
+              <p>12 MPH</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
