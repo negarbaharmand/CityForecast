de6cc9522e44892cc16624129817f3edada70af1
Negar Baharmand <negarbaharmand@outlook.com>
2023-06-26 15:36:22 +0200

    Axios get data by location

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import { useState } from "react";
 // import axios from axios;
 
 function App() {
-  //const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${import.meta.env.VITE_API_KEY} `;
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q={location}&appid=${
+    import.meta.env.VITE_API_KEY
+  } `;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
   return (
     <>
       <div className="app">
